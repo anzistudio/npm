5b532ac565d0cf2cf18b272166ef54250aa5eb65
Anzi Studio <anzistudio.my.id@gmail.com>
2025-03-09 22:18:21 +0700

    Create rd.js

---
+++ rd.js
@@ -0,0 +1,78 @@
+const App = () => {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "app" }, /*#__PURE__*/
+    React.createElement("body", { class: "" }, /*#__PURE__*/
+    React.createElement("header", null, /*#__PURE__*/
+    React.createElement("div", { class: "inner" }, /*#__PURE__*/
+    React.createElement("div", { class: "logo" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/wwf-logo.png" })), /*#__PURE__*/
+
+    React.createElement("div", { class: "burger" }), /*#__PURE__*/
+    React.createElement("nav", null, /*#__PURE__*/
+    React.createElement("a", { class: "active", href: "#" }, "Species"), /*#__PURE__*/
+
+
+    React.createElement("a", { href: "#" }, "About Us"), /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "Our Work"), /*#__PURE__*/
+    React.createElement("a", { href: "#" }, "Get Involved")), /*#__PURE__*/
+
+    React.createElement("a", { href: "#", class: "donate-link" }, "Donate"))), /*#__PURE__*/
+
+
+
+
+
+    React.createElement("main", null, /*#__PURE__*/
+    React.createElement("div", { id: "slider" }, /*#__PURE__*/
+    React.createElement("div", { class: "slider-inner" }, /*#__PURE__*/
+    React.createElement("div", { id: "slider-content" }, /*#__PURE__*/
+    React.createElement("div", { class: "meta" }, "Species"), /*#__PURE__*/
+    React.createElement("h2", { id: "slide-title" }, "Amur ", /*#__PURE__*/
+    React.createElement("br", null), "Leopard"), /*#__PURE__*/
+
+
+    React.createElement("span", { "data-slide-title": "0" }, "Amur ", /*#__PURE__*/
+    React.createElement("br", null), "Leopard"), /*#__PURE__*/
+
+
+    React.createElement("span", { "data-slide-title": "1" }, "Asiatic ", /*#__PURE__*/
+    React.createElement("br", null), "Lion"), /*#__PURE__*/
+
+
+    React.createElement("span", { "data-slide-title": "2" }, "Siberian ", /*#__PURE__*/
+    React.createElement("br", null), "Tiger"), /*#__PURE__*/
+
+
+    React.createElement("span", { "data-slide-title": "3" }, "Brown ", /*#__PURE__*/
+    React.createElement("br", null), "Bear"), /*#__PURE__*/
+
+
+    React.createElement("div", { class: "meta" }, "Status"), /*#__PURE__*/
+    React.createElement("div", { id: "slide-status" }, "Critically Endangered"), /*#__PURE__*/
+    React.createElement("span", { "data-slide-status": "0" }, "Critically Endangered"), /*#__PURE__*/
+    React.createElement("span", { "data-slide-status": "1" }, "Endangered"), /*#__PURE__*/
+    React.createElement("span", { "data-slide-status": "2" }, "Endangered"), /*#__PURE__*/
+    React.createElement("span", { "data-slide-status": "3" }, "Least Concern"))), /*#__PURE__*/
+
+
+
+    React.createElement("img", { src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/leopard2.jpg" }), /*#__PURE__*/
+    React.createElement("img", { src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/lion2.jpg" }), /*#__PURE__*/
+    React.createElement("img", { src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/tiger2.jpg" }), /*#__PURE__*/
+    React.createElement("img", { src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/bear2.jpg" }), /*#__PURE__*/
+
+    React.createElement("div", { id: "pagination" }, /*#__PURE__*/
+    React.createElement("button", { class: "active", "data-slide": "0" }), /*#__PURE__*/
+    React.createElement("button", { "data-slide": "1" }), /*#__PURE__*/
+    React.createElement("button", { "data-slide": "2" }), /*#__PURE__*/
+    React.createElement("button", { "data-slide": "3" })))))));
+
+
+
+
+
+
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), rootElement);
